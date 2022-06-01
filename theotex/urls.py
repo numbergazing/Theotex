7c37d7d7480343810dd8ba5f833df762bc6fe232
numbergazing <manoahbernier@gmail.com>
2022-06-01 19:29:00 +0200

    Fixing errors in url variables and construct_chapter_url

--- theotex/urls.py
+++ theotex/urls.py
@@ -3,10 +3,10 @@ from typing import Union
 from theotex import Corpus, SeptuagintBook, NewTestamentBook
 
 HOME = "https://theotex.org"
-SEPTUAGINT = f"{HOME}/{Corpus.SEPTUAGINT}"
-SEPTUAGINT_CHAPTER = SEPTUAGINT.join("/{book}/{book}_{chapter_nb}.html")
-NEW_TESTAMENT = f"{HOME}/{Corpus.NEW_TESTAMENT}"
-NEW_TESTAMENT_CHAPTER = NEW_TESTAMENT.join("/{book}/{book}_{chapter_nb}_gf.html")
+SEPTUAGINT = f"{HOME}/{Corpus.SEPTUAGINT.value}"
+SEPTUAGINT_CHAPTER = SEPTUAGINT + "/{book}/{book}_{chapter_nb}.html"
+NEW_TESTAMENT = f"{HOME}/{Corpus.NEW_TESTAMENT.value}"
+NEW_TESTAMENT_CHAPTER = NEW_TESTAMENT + "/{book}/{book}_{chapter_nb}_gf.html"
 
 Book = Union[SeptuagintBook, NewTestamentBook]
 
@@ -15,7 +15,7 @@ def construct_chapter_url(corpus: Corpus, book: Book, chapter: int) -> str:
 
     url: str
 
-    if corpus is Corpus.SEPTUAGINT:
+    if type(corpus) is Corpus.SEPTUAGINT:
         url = SEPTUAGINT_CHAPTER
     else:
         url = NEW_TESTAMENT_CHAPTER
