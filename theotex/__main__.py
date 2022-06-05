1b7e12bf98f9661654e868c914a2554b7b9be9ee
numbergazing <manoahbernier@gmail.com>
2022-06-05 20:42:34 +0200

    Adding two options to filter verses by language - Using sys.exit to terminate the program

--- theotex/__main__.py
+++ theotex/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
 import argparse
 
 from theotex import _slugify, _get_book
@@ -21,6 +22,8 @@ def _init_argparse() -> argparse.ArgumentParser:
         metavar="",
         help="display verses referenced with [book] [chapter] [verse] or [verse:verse]"
     )
+    parser.add_argument("--only-french", action="store_true", default=False, required=False)
+    parser.add_argument("--only-greek", action="store_true", default=False, required=False)
 
     return parser
 
@@ -40,16 +43,42 @@ def main():
         book_slug = _slugify(args.get_verses[0])
         verse_refs = args.get_verses[2].split(":")
 
+        if args.only_french is True and args.only_greek is True:
+            sys.exit("You can only use one of these options (--only-french, --only-greek) at a time.")
+
         if len(verse_refs) > 1:
+
+            to_erase_index: int
+
+            if args.only_french:
+                to_erase_index = -1
+            elif args.only_greek:
+                to_erase_index = -1
+            else:
+                to_erase_index = -2
+
             for verse in get_verses_for(_get_book(book_slug), int(args.get_verses[1]), verse_refs):
-                message += f"{str(verse)}\n\n"
-            message = message[:-2]
+                if args.only_french:
+                    message += f"{verse.get_french_str}\n"
+                elif args.only_greek:
+                    message += f"{verse.get_greek_str}\n"
+                else:
+                    message += f"{str(verse)}\n\n"
+
+            message = message[:to_erase_index]
+
         else:
             verse = get_verse_for(_get_book(book_slug), int(args.get_verses[1]), verse_refs[0])
-            message = str(verse)
+            if args.only_french:
+                message = verse.get_french_str
+            elif args.only_greek:
+                message = verse.get_greek_str
+            else:
+                message = str(verse)
 
     print(f"{message}")
     return message
 
 
 main()
+sys.exit(0)
