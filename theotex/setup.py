ab73e5828fbc10b75cf3fe02a5b969f714bda146
numbergazing <manoahbernier@gmail.com>
2022-06-07 14:50:46 +0200

    Adding a setup file for later publication

---
+++ theotex/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='theotex',
+    version='1.0.0',
+    packages=[''],
+    package_dir={'': 'theotex'},
+    url='https://github.com/numbergazing/Theotex',
+    license='MIT',
+    author='numbergazing',
+    author_email='hello@numbergazing.com',
+    description='A python package to get Bible verses from https://theotex.org'
+)
