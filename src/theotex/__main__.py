74778bdf637d5b27cd9e98087808db4e206268ef
numbergazing <manoahbernier@gmail.com>
2022-06-09 17:52:34 +0200

    Migrating argument parsing from argparse to click

--- src/theotex/__main__.py
+++ src/theotex/__main__.py
@@ -1,96 +1,91 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import sys
-import argparse
 from string import digits
 from typing import List
 
-from theotex import _slugify, _get_book, CorpusName, SeptuagintBookName, NewTestamentBookName, __version__
+import click
+
+import theotex
+from theotex import _slugify, _get_book, CorpusName, SeptuagintBookName, NewTestamentBookName
 from theotex.navigation import get_verse_for, get_verses_for
 
 
-def _init_argparse() -> argparse.ArgumentParser:
-
-    parser = argparse.ArgumentParser(
-        prog="theotex",
-        usage="%(prog)s [OPTIONS]",
-        description="Print Bible verses in French and Greek from https://theotex.org."
-    )
-    parser.add_argument("-v", "--version", action="version", version=f"%(prog)s version {__version__}")
-    parser.add_argument(
-        "-s", "--seek",
-        action="store",
-        nargs=3,
-        metavar="",
-        help="display verses (usage examples: -s Jean 3 16 or --seek Proverbes 3 5:6)"
-    )
-    parser.add_argument(
-        "-f", "--french-only",
-        action="store_true",
-        default=False,
-        help="display the verses in french only."
-    )
-    parser.add_argument(
-        "-g", "--greek-only",
-        action="store_true",
-        default=False,
-        help="display the verses in greek only."
-    )
-    parser.add_argument(
-        "-c", "--corpora",
-        action="store_true",
-        default=False,
-        help="display a list of the available corpora."
-    )
-    parser.add_argument(
-        "-b", "--books",
-        action="store",
-        nargs=1,
-        metavar="",
-        help="display a list of the available books for a corpus (usage example: -b Septante)."
-    )
-
-    return parser
+def print_version(ctx, _, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(f"""
+    theotex, version {theotex.__version__}
+    
+    Datasource: https://theotex.org
+    Developer: numbergazing
+    Email: hello@numbergazing.com
+    """)
+    ctx.exit()
 
 
-def _corpora() -> str:
+@click.group(context_settings=dict(help_option_names=['-h', '--help']))
+@click.option(
+    "-v", "--version",
+    is_flag=True,
+    callback=print_version,
+    expose_value=False,
+    is_eager=True,
+    help="Output version information and exit"
+)
+def _theotex():
+    """Print Bible verses in French and Greek from https://theotex.org."""
+    pass
+
+
+@_theotex.command()
+def corpora() -> None:
+    """Display a list of the available corpora."""
 
     corpora: List[str]
     message: str
 
     corpora = [member.value for member in CorpusName.__members__.values()]
     message = "\n".join(corpora)
-    print(message)
-
-    return message
+    click.echo(message)
 
 
-def _books(args: argparse.Namespace) -> str:
+@_theotex.command()
+@click.argument("corpus", type=click.STRING)
+def books(corpus: str) -> None:
+    """Display a list of the available books for a corpus."""
 
-    slugified_corpus: str
+    corpus_slug: str
     corpora: List[str]
     books: List[str]
+    message: str
 
-    slugified_corpus = _slugify(args.books[0])
+    corpus_slug = _slugify(corpus)
     corpora = [_slugify(member.value) for member in CorpusName.__members__.values()]
 
-    if slugified_corpus not in corpora:
-        sys.exit(f"The corpus {args.books[0]} does not exist.")
+    if corpus_slug not in corpora:
+        sys.exit(f"The corpus {corpus} does not exist.")
 
-    match slugified_corpus:
+    match corpus_slug:
         case "septante":
             books = [member.value for member in SeptuagintBookName.__members__.values()]
         case "nouveau_testament":
             books = [member.value for member in NewTestamentBookName.__members__.values()]
 
     message = "\n".join(books)
-    print(message)
-
-    return message
+    click.echo(message)
 
 
-def _seek(args: argparse.Namespace) -> str:
+@_theotex.command()
+@click.argument("book", type=click.STRING)
+@click.argument("chapter", type=click.INT)
+@click.argument("verses", type=click.STRING, metavar="[VERSE | VERSE:VERSE]")
+@click.option("-f", "--french-only", is_flag=True, default=False, help="Display only the french verses.")
+@click.option("-g", "--greek-only", is_flag=True, default=False, help="Display only the greek verses.")
+def seek(book: str, chapter: int, verses: str, french_only: bool, greek_only: bool) -> None:
+    """
+        Display verses (theotex seek -h, for more info).
+
+        Examples: Jean 3 16, Proverbes 3 5:6
+    """
 
     message: str
     refs: List[str]
@@ -99,33 +94,32 @@ def _seek(args: argparse.Namespace) -> str:
     filtered_refs: set
 
     message = str()
-    refs = args.seek
-    book_slug = _slugify(refs[0])
-    verse_refs = refs[2].split(":")
+    book_slug = _slugify(book)
+    verse_refs = verses.split(":")
     filtered_refs = set(verse_refs) - {""}
     verse_refs = sorted(list(filtered_refs))
 
     if book_slug.startswith(tuple([digit for digit in digits])):
         book_slug = book_slug[2:] + book_slug[0]
 
-    if args.french_only is True and args.greek_only is True:
+    if french_only is True and greek_only is True:
         sys.exit("You can only use one of these options (--only-french, --only-greek) at a time.")
 
     if len(verse_refs) > 1:
 
         to_erase_index: int
 
-        if args.french_only:
+        if french_only:
             to_erase_index = -1
-        elif args.greek_only:
+        elif greek_only:
             to_erase_index = -1
         else:
             to_erase_index = -2
 
-        for verse in get_verses_for(_get_book(book_slug), int(refs[1]), verse_refs):
-            if args.french_only:
+        for verse in get_verses_for(_get_book(book_slug), chapter, verse_refs):
+            if french_only:
                 message += f"{verse.get_french_str}\n"
-            elif args.greek_only:
+            elif greek_only:
                 message += f"{verse.get_greek_str}\n"
             else:
                 message += f"{str(verse)}\n\n"
@@ -133,37 +127,18 @@ def _seek(args: argparse.Namespace) -> str:
         message = message[:to_erase_index]
 
     else:
-        verse = get_verse_for(_get_book(book_slug), int(refs[1]), verse_refs[0])
-        if args.french_only:
+        verse = get_verse_for(_get_book(book_slug), chapter, verse_refs[0])
+        if french_only:
             message = verse.get_french_str
-        elif args.greek_only:
+        elif greek_only:
             message = verse.get_greek_str
         else:
             message = str(verse)
 
-    print(f"{message}")
-    return message
-
-
-def main():
-
-    parser: argparse.ArgumentParser
-    args: argparse.Namespace
-
-    parser = _init_argparse()
-    args = parser.parse_args()
-
-    if args.corpora is True:
-        return _corpora()
-
-    if args.books is not None:
-        return _books(args)
-
-    if args.seek is not None:
-        return _seek(args)
+    click.echo(message)
 
 
 if __name__ == "__main__":
 
-    main()
+    _theotex()
     sys.exit(0)
